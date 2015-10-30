97b98d3b83834041a76ed25527473cf83dcc9bc1
Mrcl1450 <marcelo-fil@outlook.com>
2015-10-30 14:50:54 +0000

    huashan: M bringup
    Change-Id: I7985347847f3e923cbdea0b4d74dd636c27e1035
    Signed-off-by: Mrcl1450 <marcelo-fil@outlook.com>

--- huashan.mk
+++ huashan.mk
@@ -199,11 +199,11 @@ PRODUCT_PACKAGES += \
     tinymix
 
 # FM radio
-PRODUCT_PACKAGES += \
-    FM2 \
-    FMRecord \
-    libqcomfm_jni \
-    qcom.fmradio
+#PRODUCT_PACKAGES += \
+#    FM2 \
+#    FMRecord \
+#    libqcomfm_jni \
+#    qcom.fmradio
 
 # Bluetooth
 PRODUCT_PACKAGES += \
@@ -242,11 +242,6 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf
 
-# Misc
-PRODUCT_PACKAGES += \
-    librs_jni \
-    com.android.future.usb.accessory
-
 # Lights
 PRODUCT_PACKAGES += \
     lights.msm8960
@@ -258,10 +253,6 @@ PRODUCT_PACKAGES += \
 # Thermal management
 PRODUCT_PACKAGES += \
     thermanager
-
-# Set default USB interface
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    persist.sys.usb.config=mtp
 
 # ART
 PRODUCT_PROPERTY_OVERRIDES += \
